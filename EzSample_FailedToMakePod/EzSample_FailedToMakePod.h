79e1d3f29974d911475ee7f70689ef03d3590b4b
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-05-10 12:20:40 +0900

    Initial Commit

---
+++ EzSample_FailedToMakePod/EzSample_FailedToMakePod.h
@@ -0,0 +1,19 @@
+//
+//  EzSample_FailedToMakePod.h
+//  EzSample_FailedToMakePod
+//
+//  Created by Tomohiro Kumagai on H27/05/10.
+//  Copyright (c) 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EzSample_FailedToMakePod.
+FOUNDATION_EXPORT double EzSample_FailedToMakePodVersionNumber;
+
+//! Project version string for EzSample_FailedToMakePod.
+FOUNDATION_EXPORT const unsigned char EzSample_FailedToMakePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EzSample_FailedToMakePod/PublicHeader.h>
+
+

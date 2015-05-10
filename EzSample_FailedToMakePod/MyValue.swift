4d9ef63c4017355e61711987c750fb575ea743b9
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-05-10 13:27:51 +0900

    Modified.

--- EzSample_FailedToMakePod/MyValue.swift
+++ EzSample_FailedToMakePod/MyValue.swift
@@ -11,12 +11,16 @@ public struct MyValue<T:AnyObject> {
 	public typealias Equal = (T, T) -> Bool
 	public typealias Containers = ContiguousArray<String>
 	
-//	public private(set) var equal:Equal
+	public private(set) var equal:Equal
 	public private(set) var containers:Containers
 	
-	public init(equal: Equal) {
+	/*!
+	Failed to make pod when the instance have properties both `containers` and `equal`.
+	If the type of the `containers` is Array<String>, no error occured during making pod.
+	*/
+	public init() {
 		
 		self.containers = Containers()
-//		self.equal = equal
+		self.equal = { $0 === $1 }
 	}
 }
